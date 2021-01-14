6d16211d3beda67d278f665ff4983d6e5cc9073f
Shayne Thiessen <shayne@shaynethiessen.com>
2021-01-14 09:33:24 -0600

    Restructured Project

--- packages/web/src/Weather/Weather.tsx
+++ packages/web/src/Weather/Weather.tsx
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import {Header, Image, Table, TableBody, TableCell, TableRow} from 'semantic-ui-react';
-import {ForecastType} from '../../Types';
+import {ForecastType} from '../../../types';
 import {environment} from '../environment';
-import {Forecast} from '../../Server/Weather';
+import {Forecast} from '../../../server/Weather';
 
 export function Weather() {
 	const {lat, lon, apiKey, units} = environment;
