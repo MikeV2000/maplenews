c348900cd7a7c371ba5e87d352b6100768bf8cca
Shayne Thiessen <shayne@shaynethiessen.com>
2021-01-20 15:00:37 -0600

    Added mongodb

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import {connect} from 'mongodb';
 import {endpoints} from './core/endpoints';
 import {environment} from './core/environment';
 
@@ -8,16 +9,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-endpoints.forEach(endpoint => {
-	app.post(`/${endpoint.path}`, (req, res) => {
-		endpoint.function(req.body.data).then(data => res.send(data));
-	});
-});
+connect(environment.databaseURL, {useUnifiedTopology: true})
+	.then(client => {
+		const db = client.db(environment.databaseName);
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+		endpoints.forEach(endpoint => {
+			app.post(`/${endpoint.path}`, (req, res) => {
+				endpoint.function(req.body.data).then(data => res.send(data));
+			});
+		});
 
-app.listen(environment.serverPORT, () => {
+		app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+
+		app.listen(environment.serverPORT, () => {
+			// TODO issue #54
+			// eslint-disable-next-line
+			console.log(`${environment.siteTitle}: Server is running at ${environment.serverUrl}`);
+		});
+	})
 	// TODO issue #54
 	// eslint-disable-next-line
-	console.log(`${environment.siteTitle}: Server is running at ${environment.serverUrl}`);
-});
+	.catch(error => console.log(error));
